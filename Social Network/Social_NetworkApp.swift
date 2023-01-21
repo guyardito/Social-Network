6fb913b922435aac516f1dcc506016761c0d47d5
guyardito <guyardito@gmail.com>
2023-01-21 09:21:48 -0500

    Initial Commit

---
+++ Social Network/Social_NetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_NetworkApp.swift
+//  Social Network
+//
+//  Created by Dev on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Social_NetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
